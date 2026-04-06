88226cd8619165734f1727411f6c1e1da7329f30
CL4-Bisk <jcaparicio@up.edu.ph>
2026-04-06 16:00:31 +0800

    feat: implemented NPC spawning

--- scenes/stages/stage.gd
+++ scenes/stages/stage.gd
@@ -3,13 +3,28 @@ class_name Stage
 
 @onready var player: Player = $Player
 @onready var access_zones: Area2D = $AccessZones
-
-@export var npc_scene : PackedScene
 @onready var base_map: NavigationRegion2D = $Map/BaseMap
 
-func _process(_delta: float) -> void:
-	if Input.is_action_just_pressed("spawn"):
+@export var npc_scene : PackedScene
+@export var max_npcs: int = 10
+@export var spawn_interval: float = 1.0 # Seconds between spawns
+
+var current_npc_count: int = 0
+var spawn_timer: Timer
+
+func _ready() -> void:
+	# Set up a timer to handle the "gradual" part
+	spawn_timer = Timer.new()
+	add_child(spawn_timer)
+	spawn_timer.wait_time = spawn_interval
+	spawn_timer.timeout.connect(_on_spawn_timer_timeout)
+	spawn_timer.start()
+
+func _on_spawn_timer_timeout() -> void:
+	if current_npc_count < max_npcs:
 		spawn_npc()
+	else:
+		spawn_timer.stop()
 
 func pick_access_point() -> Vector2:
 	var point = access_zones.get_children().pick_random()
@@ -22,8 +37,10 @@ func pick_access_point() -> Vector2:
 
 func spawn_npc() -> void:
 	var n = npc_scene.instantiate() as NPC
+	n.tree_exited.connect(func(): current_npc_count -= 1; spawn_timer.start())
 	n.global_position = pick_access_point()
 	add_child(n)
+	current_npc_count += 1
 	n.stage = self
 	n.nav2d.target_position = NavigationServer2D.region_get_random_point(base_map.get_rid(), 1, true)
 	n.start_state(&"enter")
